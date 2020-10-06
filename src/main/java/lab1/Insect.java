6f8608c5a01d0864291f4409ad93b44290a6c31a
Max <maximlev01234@gmail.com>
2020-10-06 18:20:55 +0300

    added files for both sorting algorithms

---
+++ src/main/java/lab1/Insect.java
@@ -0,0 +1,46 @@
+package lab1;
+
+public class Insect {
+    private String name;
+    private double velocityInMeters;
+    private int weightInGrams;
+
+    public Insect(String name, double velocityInMeters, int weightInGrams) {
+        this.name = name;
+        this.velocityInMeters = velocityInMeters;
+        this.weightInGrams = weightInGrams;
+    }
+
+    @Override
+    public String toString() {
+        return "Insect{" +
+                "name='" + name + '\'' +
+                ", velocityInMeters=" + velocityInMeters +
+                ", weightInGrams=" + weightInGrams +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getVelocityInMeters() {
+        return velocityInMeters;
+    }
+
+    public void setVelocityInMeters(double velocityInMeters) {
+        this.velocityInMeters = velocityInMeters;
+    }
+
+    public int getWeightInGrams() {
+        return weightInGrams;
+    }
+
+    public void setWeightInGrams(int weightInGrams) {
+        this.weightInGrams = weightInGrams;
+    }
+}
